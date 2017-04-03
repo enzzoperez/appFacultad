2af4189e0e0d458bcecdce6938a085cecad138d3
enzzo <enzzo@localhost.localdomain>
2017-04-03 12:29:26 +0200

    mabe

--- src/providers/materias.service.ts
+++ src/providers/materias.service.ts
@@ -11,9 +11,8 @@ export class MateriasService {
 
 	getMateriaDia(dia:string):Array <any>{
 		let materias = MATERIAS.filter(x => x.horarios.filter(h=>h.dia === dia));
-		
 		for(let data of materias) {
-			console.log('UNA MATERIA-->', materias.values());
+			console.log('UNA MATERIA-->', materias;
 		}
 		return materias;
 	}
